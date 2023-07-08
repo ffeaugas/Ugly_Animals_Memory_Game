fefe689a5c9d02e741be9f77ce74978c7edc0b3c
ffeaugas <ffeaugas@student.42angouleme.fr>
2023-07-08 18:02:27 +0200

    main functionalities finished

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
-const resultDisplay = document.querySelector('#result');
+const startButton = document.querySelector('button');
 
 let firstCard = null;
 let secondCard = null;
 let score = 0;
 unrevealedCardImg = 'images/cat.png';
-resultDisplay.textContent = score;
+
+startButton.addEventListener('click', () => {
+    startButton.style.display = 'none';
+    createGame();
+});
+
+async function createGame() {
+    try {
+        const urlArray = await getUrls(18);
+        urlArray.sort(() => 0.5 - Math.random())
+        const newGame = new memoryGame(urlArray);
+    }
+    catch (err) {
+        console.log('Error : ', err);
+    }
+}
 
 async function getUrls(arraySize) {
     const urlArray = [];
 
     for (let i = 0; i < arraySize; i += 2) {
-        const jsonObj = await fetch('https://cataas.com/cat?json=true');
+        const jsonObj = await fetch('https://cataas.com/cat?json=true&width=:100');
+
+        if (!jsonObj.ok)
+            throw new Error('request to cataas.com failed');
         const obj = await jsonObj.json();
         urlArray.push('https://cataas.com' + obj.url);
         urlArray.push('https://cataas.com' + obj.url);
@@ -18,31 +36,27 @@ async function getUrls(arraySize) {
     return urlArray;
 }
 
-showURL();
-
-async function showURL() {
-    try {
-        const urlArray = await getUrls(25);
-        urlArray.sort(() => 0.5 - Math.random())
-        // console.log(urlArray);
-        const newGame = new memoryGame(urlArray);
-    }
-    catch (err) {
-        console.log('nnnnnnn', err);
-    }
-}
-
 class memoryGame {
-
     constructor(urlArray) {
-        this.size = urlArray.length;
+        // Score element creation
+
+        const scoreElement = document.createElement('h2');
+        scoreElement.textContent = 'Score : ';
+        const scoreValueElement = document.createElement('span');
+        document.querySelector('main').appendChild(scoreElement);
+        scoreElement.appendChild(scoreValueElement);
+        scoreValueElement.textContent = score;
+        scoreValueElement.setAttribute('id', '#result');
+
+        // Game grid creation
 
         const newGame = document.createElement('div');
         newGame.classList.add('grid');
         document.querySelector('main').appendChild(newGame);
 
+        // Grid cards creation
+
         for (let i = 0; i < urlArray.length; i++) {
-            // console.log(urlArray[i]);
             const newCard = document.createElement('img');
             newCard.setAttribute('src', unrevealedCardImg);
             newCard.setAttribute('data-id', i);
@@ -52,6 +66,8 @@ class memoryGame {
             });
         }
     }
+
+    // Flipping card event
 
     flipCard(card, urlArray) {
         if (card.getAttribute('src') === 'images/white.png')
@@ -71,6 +87,8 @@ class memoryGame {
         }
     }
 
+    // Pair check after revealing 2 cards
+
     checkMatch(urlArray) {
         if (firstCard.getAttribute('src') === secondCard.getAttribute('src')) {
             firstCard.setAttribute('src', 'images/white.png');
@@ -83,6 +101,6 @@ class memoryGame {
         firstCard = null;
         secondCard = null;
         score++;
-        resultDisplay.textContent = score;
+        document.getElementById('#result').textContent = score;
     }
 }
